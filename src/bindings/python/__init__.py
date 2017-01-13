0a0e82df16df4c0b742e096b30910fe32f6df8a4
o0u812 <o0u812@ceb27deb-1697-43c4-b304-af667b757341>
2017-01-13 01:52:12 +0000

    remove v from __version__ and add myself to authors
    git-svn-id: svn+ssh://svn.code.sf.net/p/phrasedml/code/trunk@80 ceb27deb-1697-43c4-b304-af667b757341

--- src/bindings/python/__init__.py
+++ src/bindings/python/__init__.py
@@ -1,5 +1,5 @@
 """
-The libPhrasedml library, (c) 2014-2015 Lucian Smith, Kiri Choi, and Herbert Sauro
+The libPhrasedml library, (c) 2014-2015 Lucian Smith, Kiri Choi, J Kyle Medley, and Herbert Sauro
 
 libPhrasedml is a library for Phrasedml, a human-readable,
 human-writeable simulation experiment definition language.
@@ -8,4 +8,4 @@ For more information, see http://phrasedml.sf.net/
 
 from .phrasedml import *
 
-__version__ = LIBPHRASEDML_VERSION_STRING
+__version__ = LIBPHRASEDML_VERSION_STRING[1:]
